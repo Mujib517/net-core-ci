78784d2de7e8540e1218f75b4a5a7a5ed33b4069
Mujib517 <Mujib517@gmail.com>
2018-03-23 12:11:18 +0530

    Commit to trigger build

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Net_Core_CI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello CI");
         }
     }
 }
